e6412c1ed241f84d8b6869271ba9d5424f59f24e
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-19 17:39:13 -0600

    Adds authors and author_books to test
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>

--- spec/features/user_can_see_all_books_spec.rb
+++ spec/features/user_can_see_all_books_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "user index", type: :feature do
   it "user can see all books" do
+    author_1 = Author.create(name: "Larry Niven")
     book_1 = Book.create(title: "Ringworld", pages: 430, year_published: 1970, thumbnail: "https://d2svrcwl6l7hz1.cloudfront.net/content/B00CNTUVLO/resources/0?mime=image/*")
     book_2 = Book.create(title: "The Goliath Stone", pages: 320, year_published: 2013, thumbnail: "https://images-na.ssl-images-amazon.com/images/I/51l%2BUTphB1L.jpg")
+    ab_1 = AuthorBook.create(author_id: author_1.id, book_id: book_1.id)
+    ab_2 = AuthorBook.create(author_id: author_1.id, book_id: book_2.id)
 
     visit '/books'
+
+    save_and_open_page
 
     expect(page).to have_content(book_1.title)
     expect(page).to have_content("Pages: #{book_1.pages}")
