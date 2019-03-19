81b24c5deb51fa6bb67de9a3e53f148c3bcba5f0
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-19 16:37:49 -0600

    Created feature test for User Story 6
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>

---
+++ spec/features/user_can_see_all_books_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "user index", type: :feature do
+  it "user can see all books" do
+    book_1 = Book.create(title: "Ringworld", pages: 430, year_published: 1970, thumbnail: "https://d2svrcwl6l7hz1.cloudfront.net/content/B00CNTUVLO/resources/0?mime=image/*")
+    book_2 = Book.create(title: "The Goliath Stone", pages: 320, year_published: 2013, thumbnail: "https://images-na.ssl-images-amazon.com/images/I/51l%2BUTphB1L.jpg")
+
+    visit '/books'
+
+    expect(page).to have_content(book_1.title)
+    expect(page).to have_content("Pages: #{book_1.pages}")
+    expect(page).to have_content("Year Published: #{book_1.year_published}")
+    expect(page).to have_css("img[src*='#{book_1.thumbnail}']")
+
+    expect(page).to have_content(book_2.title)
+    expect(page).to have_content("Pages: #{book_2.pages}")
+    expect(page).to have_content("Year Published: #{book_2.year_published}")
+    expect(page).to have_css("img[src*='#{book_2.thumbnail}']")
+  end
+
+end
