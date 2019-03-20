5a5a92be944dd9cadd86e88b452b185d0c5322c7
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-20 16:14:45 -0600

    Adds validations for reviews
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :book
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :user, presence: true
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
 end
