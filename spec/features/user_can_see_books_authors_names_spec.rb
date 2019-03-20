cdeb149cf92fd6a806061d721bfb571001795887
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-03-20 12:06:31 -0600

    Add test to ensure multiple authors are shown
    Co-authored-by: Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>

--- spec/features/user_can_see_books_authors_names_spec.rb
+++ spec/features/user_can_see_books_authors_names_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe "user index", type: :feature do
     expect(page).to have_content(author_1.name)
   end
 
+  it "user can see all authors if book has multiple" do
+    author_1 = Author.create(name: "Larry Niven")
+    author_2 = Author.create(name: "Jerry Pournelle")
+    book_3 = Book.create(title: "Inferno", pages: 237, year_published: 1976,thumbnail: "https://upload.wikimedia.org/wikipedia/en/8/86/InfernoNovel.jpg")
+    ab_3 = AuthorBook.create(author_id: author_1.id, book_id: book_3.id)
+    ab_4 = AuthorBook.create(author_id: author_2.id, book_id: book_3.id)
+
+    visit books_path
+
+    expect(page).to have_content(author_1.name)
+    expect(page).to have_content(author_2.name)
+  end
+
 end
