3e0d39c43d3a928c7fcaa5f0d0ee418b44092abb
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-20 17:51:42 -0600

    Created user flow to add a new book with 1 author
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,25 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    # authors = book_params[:authors].split(',').map do |author|
+    author = Author.create(name: book_params[:authors])
+    author.books.create(title: book_params[:title],
+                    pages: book_params[:pages],
+                    year_published: book_params[:year_published],
+                    thumbnail: book_params[:thumbnail])
+
+    redirect_to book_path(author.books.first)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :authors, :pages, :year_published, :thumbnail)
+  end
+
 end
