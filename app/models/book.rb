572513afe301bad0db10e1893aeac8952f6b8720
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-03-20 17:37:53 -0600

    Add methods to get best and worst 3 books
    Co-authored-by: Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,25 @@ class Book < ApplicationRecord
   has_many :author_books
   has_many :authors, through: :author_books
   has_many :reviews
+
+  def self.by_average_ratings(**args)
+    query = self.select('books.*, AVG(reviews.rating) AS avg_rating')
+        .joins(:reviews)
+        .group('books.id')
+        .order('avg_rating ' + (args[:direction] ||= 'ASC'))
+    return query if args[:limit] === false
+    query.limit(args[:limit] ||= 3)
+  end
+
+  def self.best_3
+    by_average_ratings(direction: 'DESC', limit: 3)
+  end
+
+  def self.worst_3
+    by_average_ratings(direction: 'ASC', limit: 3)
+  end
+
+  def average_rating
+    self.reviews.average(:rating)
+  end
 end
