d35fbdfd56017fcbe45cb3cb3d6bbc7a1c05c8d9
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-03-19 21:30:56 -0600

    Add seed data and CSV import logic
    Co-authored-by: Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+authors = {}
+books = {}
+
+options = {headers: true, header_converters: :symbol}
+
+CSV.foreach('./db/authors.csv', options) do |row|
+  author = row.to_h
+  authors[author[:name]] = Author.create(author)
+end
+
+CSV.foreach('./db/books.csv', options) do |row|
+  book = row.to_h.except(:authors, :reviews)
+  books[book[:title]] = Book.create(book)
+  row[:authors].split(',').each do |author|
+    AuthorBook.create(book_id: books[book[:title]].id,
+                    author_id: authors[author].id)
+  end
+end
+
+CSV.foreach('./db/reviews.csv', options) do |row|
+  review = row.to_h.except(:book)
+  review[:book] = books[row[:book]]
+  Review.create(review)
+end
