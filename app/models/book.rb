826f3d1eb3ac6f7c1c71505d8a9a2791a21a04b7
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-20 15:52:23 -0600

    Adds method to calculate average rating
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
   has_many :author_books
   has_many :authors, through: :author_books
   has_many :reviews
+
+  def average_rating
+    self.reviews.average(:rating)
+  end
+
 end
