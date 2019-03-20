8bf63f3eb1e6c3575e794be2dcd6c654588b3702
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-20 15:10:34 -0600

    Adds validations for book class
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
   has_many :author_books
   has_many :authors, through: :author_books
   has_many :reviews
+  validates :title, presence: true
+  validates :pages, presence: true, numericality: { greater_than: 0 }
+  validates :year_published, presence: true, numericality: { greater_than: 0 }
 end
