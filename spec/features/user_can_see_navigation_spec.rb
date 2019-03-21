4b78834e4cc2def354ac715f2342bf00d018d254
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-21 17:10:33 -0600

    Creates test for user navigation

---
+++ spec/features/user_can_see_navigation_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "navigation", type: :feature do
+  describe 'when a user visits a webpage' do
+    describe 'they can see a navigation bar'
+      it 'links to home' do
+        visit books_path
+
+        within 'nav' do
+          click_on("Home")
+          expect(current_path).to eq('/')
+        end
+      end
+
+      it 'links to browse all books' do
+        book = Book.create(title: "Title", pages: 123, year_published: 1793)
+        visit books_path
+
+        within 'nav' do
+          click_on("Browse All Books")
+          expect(current_path).to eq(books_path)
+        end
+      end
+  end
+end
