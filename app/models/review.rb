2d498f7c5dd9be0af4b9bb181149ee514e99864b
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-03-21 11:58:01 -0600

    Add query for top reviewers to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,10 @@ class Review < ApplicationRecord
   validates :description, presence: true
   validates :user, presence: true
   validates :rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
+
+  def self.top_reviewers
+    reviewers = self.group('reviews.user')
+                    .count
+    reviewers.sort_by{|k,v|-v}.first(3)
+  end
 end
