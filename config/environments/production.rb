6082da536a5a8c9ec861c4ba11e7fcb7dec16ef9
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-15 10:26:42 -0600

    Add support for serving assets via Cloudfront
    Co-authored-by: Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000'
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -33,7 +36,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'd16aeyzkxy6v1m.cloudfront.net'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
